7a2f78a194cd1b3fdf27c91cc1e115dde6583def
javier <131582155+Namra14@users.noreply.github.com>
2023-04-24 10:06:00 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
